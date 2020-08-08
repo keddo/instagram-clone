9817c2f2630dc0991fdbb85b2be4e3ca57744af2
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-08 10:48:13 +0300

    add app title and favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Initial clean Instagram clone app.</h1>
+      <div className="app__header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram Logo"
+          className="app__headerImage"
+        />
+      </div>
     </div>
   );
 }
