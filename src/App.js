15d6fab4668173e7221f5889d68197b7b8ced137
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-08 12:28:30 +0300

    Pass data through react props

--- src/App.js
+++ src/App.js
@@ -17,9 +17,21 @@ function App() {
       <h1>Hello programmers, Let's build Instagram Clone.</h1>
 
       {/* Posts */}
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        username="Rahef Qazi"
+        caption="Woow day three of live session."
+        imageUrl="https://instagram.fjib1-2.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/117041974_302702244497038_500430435947338940_n.jpg?_nc_ht=instagram.fjib1-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=ndlEnHZGeYYAX_Rd9CG&oh=406dc78e256ac1631f12918759d3e76d&oe=5F58339E"
+      />
+      <Post
+        username="Rahef Qazi"
+        caption="Woow day three of live session."
+        imageUrl="https://instagram.fjib1-2.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/117041974_302702244497038_500430435947338940_n.jpg?_nc_ht=instagram.fjib1-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=ndlEnHZGeYYAX_Rd9CG&oh=406dc78e256ac1631f12918759d3e76d&oe=5F58339E"
+      />
+      <Post
+        username="Rahef Qazi"
+        caption="Woow day three of live session."
+        imageUrl="https://instagram.fjib1-2.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/117041974_302702244497038_500430435947338940_n.jpg?_nc_ht=instagram.fjib1-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=ndlEnHZGeYYAX_Rd9CG&oh=406dc78e256ac1631f12918759d3e76d&oe=5F58339E"
+      />
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,27 +1,23 @@
 import React from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
-function Post() {
+function Post({ username, caption, imageUrl }) {
   return (
     <div className="post">
       <div className="post__header">
         <Avatar
           className="post__avatar"
-          alt="Rafeh Qazi"
+          alt={username}
           src="/static/images/avatar/1.jpg"
         />
         {/* header -> avatar + username */}
         <h3>Rafeh Qazi</h3>
       </div>
       {/* Image */}
-      <img
-        className="post__image"
-        src="https://instagram.fjib1-2.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/117041974_302702244497038_500430435947338940_n.jpg?_nc_ht=instagram.fjib1-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=ndlEnHZGeYYAX_Rd9CG&oh=406dc78e256ac1631f12918759d3e76d&oe=5F58339E"
-        alt="Post user"
-      />
+      <img className="post__image" src={imageUrl} alt="Post user" />
       {/* username + caption */}
       <h4 className="post__text">
-        <strong>cleveqazi:</strong> Woow day three of live session.
+        <strong>cleveqazi:</strong> {caption}
       </h4>
     </div>
   );
