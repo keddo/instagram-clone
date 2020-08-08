3406a39e5dbc9f652f25fc5ad214469741d229a8
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-08 11:09:05 +0300

    Add post component

---
+++ src/Post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            {/* header -> avatar + username */}
+            {/* Image */}
+            {/* username + caption */}
+        </div>
+    )
+}
+
+export default Post
