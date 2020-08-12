c4dee4ebd672944ff76608becce5326a730b179c
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-12 11:38:05 +0300

    Add function to handle on state change

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Button } from "@material-ui/core";
 
 function ImageUpload() {
+  const [caption, setCaption] = useState("");
   return (
     <div>
       {/* caption */}
-      <input type="text" placeholder="Enter a caption..." />
+      <input
+        type="text"
+        placeholder="Enter a caption..."
+        onChange={(ev) => setCaption(ev.target.value)}
+        value={caption}
+      />
       {/* image upload */}
       <input type="file" onChange={handleChange} />
       {/* submit buttton */}
-      <Button onClick={handleUpload}></Button>
+      <Button onClick={handleUpload}>Upload</Button>
     </div>
   );
 }
