fd8ea3f46f1c5a2dd77569b88ce3b2ff049eabe8
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-13 08:45:35 +0300

    Add snapshot listener for posts

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
+import { db } from "./firebase";
 function Post({ postId, username, caption, imageUrl }) {
-  const [comments, setComment] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("posts")
+        .doc(postId)
+        .collection("comments")
+        .onSnapshot((snap) => {
+          setComments(snap.docs.map((doc) => doc.data()));
+        });
+    }
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
   return (
     <div className="post">
       <div className="post__header">
