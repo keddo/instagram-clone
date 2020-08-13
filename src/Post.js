f7d5e037a994e06b963ad1617057265138cccedb
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-13 09:41:48 +0300

    Order comments according the they are posted.

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
 import { db } from "./firebase";
+import firebase from "firebase";
 function Post({ postId, user, username, caption, imageUrl }) {
   const [comments, setComments] = useState([]);
   const [comment, setComment] = useState("");
@@ -13,6 +14,7 @@ function Post({ postId, user, username, caption, imageUrl }) {
         .collection("posts")
         .doc(postId)
         .collection("comments")
+        .orderBy("timestamp", "desc")
         .onSnapshot((snap) => {
           setComments(snap.docs.map((doc) => doc.data()));
         });
@@ -27,6 +29,7 @@ function Post({ postId, user, username, caption, imageUrl }) {
     db.collection("posts").doc(postId).collection("comments").add({
       text: comment,
       username: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
     setComment("");
   };
